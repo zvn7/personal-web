627746b2f1b57840352a191c75dd53a059b85a88
zvn7 <aammaul7@gmail.com>
2024-07-01 22:32:21 +0700

    change length skeleton

--- src/components/molecules/SkeletonCard.tsx
+++ src/components/molecules/SkeletonCard.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function SkeletonCard() {
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-			{Array.from({ length: 8 }).map((_, index) => (
+			{Array.from({ length: 1 }).map((_, index) => (
 				<div key={index} className="relative group block p-2 h-full w-[250px]">
 					<div className="rounded-2xl h-full w-full overflow-hidden bg-black border border-white dark:border-white/[0.2] relative z-20">
 						<div className="relative z-50">
