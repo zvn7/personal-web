fa35c18a5a049eb772ddee1a917d4a254529593d
zvn7 <aammaul7@gmail.com>
2024-07-11 11:26:59 +0700

    add data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -15,6 +15,19 @@ export const blogPosts = [
 	{
 		id: 2,
 		image:
+			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgvsn-Zz3_HQeNh1pAjmjDhM-fQba3xk0EFzi78Vce_-WucfPvpJjr7z_URWuRdLU5wxsViifKyRl7pvVxznhy1ZIaFDzVlDUh3-KArCMzrsDroG2WLMvrkLX-U_jCxWhAN1V1H11h3sr-W-ivYAgfwneofdazN2F5HLfXWBgM8NqqkNdaiLtVqEkFzVLo/w400-h400/asset.jpg",
+		avatar: "https://plchldr.co/i/50x50",
+		category: "Technology",
+		date: "June 29, 2024",
+		title:
+			"Mencapai Keahlian Developer yang Handal dengan Bantuan Codepolitan dan Alibaba Cloud",
+		content:
+			"Dunia teknologi semakin berkembang pesat, dan menjadi seorang pengembang perangkat lunak yang handal merupakan salah satu kebutuhan utama dalam industri ini. Namun, bagi sebagian orang, mencapai tingkat keahlian tersebut bisa menjadi tantangan yang menakutkan. Tidak perlu khawatir! Bersama Codepolitan dan Alibaba Cloud, perjalanan Anda menuju keahlian pengembangan yang handal bisa menjadi lebih mudah dan menyenangkan.",
+		link: "https://createdzvn.blogspot.com/2024/03/mencapai-keahlian-developer-yang-handal.html",
+	},
+	{
+		id: 3,
+		image:
 			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgQVz7ZuwxLvW708OradCP0Azc7ZzWmlR1nmHKr9w7FRJFE4xzRTDTWiSo7WG069zPFpaCH6xcNu5bvFUFs9b4O4wyC4-rmfk9fHbtuenCVeRj5XnHBvw49aFg3kedG2TrzI9mnmmUGIQ9hxVu34_GlBsMT8wgooJjC9dbZh9qJDeEpUHOdJv_TFOjJwLs/w400-h225/Blog%20Banner.png",
 		avatar: "https://plchldr.co/i/50x50",
 		category: "Travel",
@@ -25,91 +38,52 @@ export const blogPosts = [
 		link: "https://createdzvn.blogspot.com/2023/09/belajar-html-dengan-mudah-panduan.html",
 	},
 	{
-		id: 3,
-		image:
-			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgvsn-Zz3_HQeNh1pAjmjDhM-fQba3xk0EFzi78Vce_-WucfPvpJjr7z_URWuRdLU5wxsViifKyRl7pvVxznhy1ZIaFDzVlDUh3-KArCMzrsDroG2WLMvrkLX-U_jCxWhAN1V1H11h3sr-W-ivYAgfwneofdazN2F5HLfXWBgM8NqqkNdaiLtVqEkFzVLo/w400-h400/asset.jpg",
-		avatar: "https://plchldr.co/i/50x50",
-		category: "Technology",
-		date: "June 29, 2024",
-		title:
-			"Saya Memilih Berprofesi di Bidang IT Karena? dan Mengikuti Program Devhandal untuk Mendukung Tujuan Saya",
-		content:
-			"Teknologi informasi (IT) telah menjadi bagian tak terpisahkan dari kehidupan kita sehari-hari. Dari ponsel pintar yang kita gunakan untuk berkomunikasi hingga aplikasi yang membantu kita bekerja dan belajar, IT ada di mana-mana. Seiring perkembangan teknologi, kebutuhan akan profesional IT terus meningkat. Saya memilih berprofesi di bidang IT karena berbagai alasan, dan salah satu langkah yang saya ambil untuk mencapai tujuan saya adalah mengikuti program Devhandal.",
-		link: "https://createdzvn.blogspot.com/2024/06/saya-memilih-berprofesi-di-bidang-it.html",
-	},
-	{
 		id: 4,
 		image:
-			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgvsn-Zz3_HQeNh1pAjmjDhM-fQba3xk0EFzi78Vce_-WucfPvpJjr7z_URWuRdLU5wxsViifKyRl7pvVxznhy1ZIaFDzVlDUh3-KArCMzrsDroG2WLMvrkLX-U_jCxWhAN1V1H11h3sr-W-ivYAgfwneofdazN2F5HLfXWBgM8NqqkNdaiLtVqEkFzVLo/w400-h400/asset.jpg",
+			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhCdmPXaF4wIyOh4UHjQ5AL3EV4-3En0EOEeHJgsie4i4g5uS7lCCFgVtL1Cr8wxfsVuPsfPiifVH95pPQULn-wVqDU60RU-2cwq0_cm4vMgjA5DmVXBGAtrqvMz4o4uYp6hTfi3ySjvxDLB_Y_mQTr9w00bQE5WCYkRsDG0Cvu34TvLNsn1re7rUMPx6w/s4480/20230912_222400_0000.png",
 		avatar: "https://plchldr.co/i/50x50",
 		category: "Technology",
 		date: "June 29, 2024",
-		title:
-			"Saya Memilih Berprofesi di Bidang IT Karena? dan Mengikuti Program Devhandal untuk Mendukung Tujuan Saya",
+		title: "Memahami CSS: Fungsi, Sintaksis, dan Cara Kerja",
 		content:
-			"Teknologi informasi (IT) telah menjadi bagian tak terpisahkan dari kehidupan kita sehari-hari. Dari ponsel pintar yang kita gunakan untuk berkomunikasi hingga aplikasi yang membantu kita bekerja dan belajar, IT ada di mana-mana. Seiring perkembangan teknologi, kebutuhan akan profesional IT terus meningkat. Saya memilih berprofesi di bidang IT karena berbagai alasan, dan salah satu langkah yang saya ambil untuk mencapai tujuan saya adalah mengikuti program Devhandal.",
+			"Cascading Style Sheets (CSS) adalah elemen penting dalam dunia desain web. Dalam artikel ini, kita akan menjelajahi dunia CSS, menguraikan fungsi-fungsinya, menjelaskan sintaksis dasarnya, dan membahas cara kerja CSS dalam memberikan tampilan dan gaya pada halaman web.",
 		link: "https://createdzvn.blogspot.com/2024/06/saya-memilih-berprofesi-di-bidang-it.html",
 	},
 	{
 		id: 5,
 		image:
-			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgQVz7ZuwxLvW708OradCP0Azc7ZzWmlR1nmHKr9w7FRJFE4xzRTDTWiSo7WG069zPFpaCH6xcNu5bvFUFs9b4O4wyC4-rmfk9fHbtuenCVeRj5XnHBvw49aFg3kedG2TrzI9mnmmUGIQ9hxVu34_GlBsMT8wgooJjC9dbZh9qJDeEpUHOdJv_TFOjJwLs/w400-h225/Blog%20Banner.png",
+			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgeUT6114gh3hjbp4IyqbSgVEbmHAQOsJEz8kotzKKemA3NwCeobjKdZih075PiFTubnNxUgXXqgk_KP1xm15_jZLEaVshiRKDzcJCWgAkwwwFvNgsh2LCtZsiGGFJfCsHHUqlVkNLec9UuRmDajA6ijkyM4ycCqTxe2lO6f0C1s9OVVcLWtWRXUx5jMLY/s4480/20230908_081532_0000.png",
 		avatar: "https://plchldr.co/i/50x50",
-		category: "Travel",
-		date: "June 28, 2024",
-		title: "Belajar HTML dengan Mudah: Panduan Pemula untuk Coding Web",
+		category: "Technology",
+		date: "June 29, 2024",
+		title: "Panduan Praktis: Cara Belajar HTML dengan Efektif",
 		content:
-			"Ingin tahu langkah pertama belajar HTML, di mana belajar, dan mengapa Anda harus melakukannya? Temukan jawabannya dalam panduan ini, serta rincian tentang waktu yang dibutuhkan, perangkat yang diperlukan, dan contoh kode HTML.",
-		link: "https://createdzvn.blogspot.com/2023/09/belajar-html-dengan-mudah-panduan.html",
+			"HTML (Hypertext Markup Language) adalah bahasa markup dasar yang menjadi fondasi setiap halaman web di internet. Memahami HTML adalah langkah penting dalam perjalanan Anda menjadi seorang web developer. Artikel ini akan memberikan panduan praktis tentang cara belajar HTML dengan efektif, baik bagi pemula maupun mereka yang ingin memperdalam pengetahuannya.",
+		link: "https://createdzvn.blogspot.com/2023/09/panduan-praktis-cara-belajar-html.html",
 	},
 	{
 		id: 6,
 		image:
-			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgvsn-Zz3_HQeNh1pAjmjDhM-fQba3xk0EFzi78Vce_-WucfPvpJjr7z_URWuRdLU5wxsViifKyRl7pvVxznhy1ZIaFDzVlDUh3-KArCMzrsDroG2WLMvrkLX-U_jCxWhAN1V1H11h3sr-W-ivYAgfwneofdazN2F5HLfXWBgM8NqqkNdaiLtVqEkFzVLo/w400-h400/asset.jpg",
-		avatar: "https://plchldr.co/i/50x50",
+			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEicqkToLEHM1Xq8tWTymeL6MZ2IOzJy5lF1sr1JcaTWIm1q0Km8oWCE6tW_dXaHP_GBgxT0uXiCmxWcGRCTgE7X92DuVSaz_iJk8uTTz3VeKFP1xIZNCrLRPv9sfnFiK3LI2h_jkbUny-UPZU5Mj2fNapqKGThzhq6EkH5p1ZJrpfMu1uhiOBZ1x9G3Te8/s4480/20230908_081000_0000.png",
 		category: "Technology",
 		date: "June 29, 2024",
 		title:
-			"Saya Memilih Berprofesi di Bidang IT Karena? dan Mengikuti Program Devhandal untuk Mendukung Tujuan Saya",
+			"Menggali Dunia Bahasa Pemrograman PHP: Fungsi, Sintaks Dasar, dan Cara Kerjanya",
 		content:
-			"Teknologi informasi (IT) telah menjadi bagian tak terpisahkan dari kehidupan kita sehari-hari. Dari ponsel pintar yang kita gunakan untuk berkomunikasi hingga aplikasi yang membantu kita bekerja dan belajar, IT ada di mana-mana. Seiring perkembangan teknologi, kebutuhan akan profesional IT terus meningkat. Saya memilih berprofesi di bidang IT karena berbagai alasan, dan salah satu langkah yang saya ambil untuk mencapai tujuan saya adalah mengikuti program Devhandal.",
-		link: "https://createdzvn.blogspot.com/2024/06/saya-memilih-berprofesi-di-bidang-it.html",
+			"Dalam era digital yang semakin berkembang, bahasa pemrograman PHP (Hypertext Preprocessor) telah menjadi salah satu alat paling penting bagi pengembang web di seluruh dunia. Di artikel ini, kami akan membahas PHP, menguraikan fungsi-fungsi utamanya, menjelaskan sintaks dasarnya, dan membahas cara kerja bahasa pemrograman ini.",
+		link: "https://createdzvn.blogspot.com/2023/09/menggali-dunia-bahasa-pemrograman-php.html",
 	},
 	{
 		id: 7,
 		image:
-			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgQVz7ZuwxLvW708OradCP0Azc7ZzWmlR1nmHKr9w7FRJFE4xzRTDTWiSo7WG069zPFpaCH6xcNu5bvFUFs9b4O4wyC4-rmfk9fHbtuenCVeRj5XnHBvw49aFg3kedG2TrzI9mnmmUGIQ9hxVu34_GlBsMT8wgooJjC9dbZh9qJDeEpUHOdJv_TFOjJwLs/w400-h225/Blog%20Banner.png",
+			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiL-7HFNU4vy8BtXfSg6AbfactLyhTcZQoHpWprZa4BiCrV94MWAlpxLwqd-0pi_1ONsQRNmhmUDf1MaBbFCfXliFonMiaOK2xZvP7hPZ9IdlkndXxDIjBlUEgJP03pZi-Ab2KUjdXqhaHw_ibz1a-gyRkD3-9upqDePJnDo7QOe4qhaG8WPGqmgcfdSlo/s4480/20230830_223903_0000.png",
 		avatar: "https://plchldr.co/i/50x50",
-		category: "Travel",
-		date: "June 28, 2024",
-		title: "Belajar HTML dengan Mudah: Panduan Pemula untuk Coding Web",
+		category: "Technology",
+		date: "June 29, 2024",
+		title: "Belajar HTML: Panduan Lengkap untuk Menjadi Web Developer",
 		content:
-			"Ingin tahu langkah pertama belajar HTML, di mana belajar, dan mengapa Anda harus melakukannya? Temukan jawabannya dalam panduan ini, serta rincian tentang waktu yang dibutuhkan, perangkat yang diperlukan, dan contoh kode HTML.",
-		link: "https://createdzvn.blogspot.com/2023/09/belajar-html-dengan-mudah-panduan.html",
-	},
-	{
-		id: 8,
-		image:
-			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgQVz7ZuwxLvW708OradCP0Azc7ZzWmlR1nmHKr9w7FRJFE4xzRTDTWiSo7WG069zPFpaCH6xcNu5bvFUFs9b4O4wyC4-rmfk9fHbtuenCVeRj5XnHBvw49aFg3kedG2TrzI9mnmmUGIQ9hxVu34_GlBsMT8wgooJjC9dbZh9qJDeEpUHOdJv_TFOjJwLs/w400-h225/Blog%20Banner.png",
-		avatar: "https://plchldr.co/i/50x50",
-		category: "Travel",
-		date: "June 28, 2024",
-		title: "Belajar HTML dengan Mudah: Panduan Pemula untuk Coding Web",
-		content:
-			"Ingin tahu langkah pertama belajar HTML, di mana belajar, dan mengapa Anda harus melakukannya? Temukan jawabannya dalam panduan ini, serta rincian tentang waktu yang dibutuhkan, perangkat yang diperlukan, dan contoh kode HTML.",
-		link: "https://createdzvn.blogspot.com/2023/09/belajar-html-dengan-mudah-panduan.html",
-	},
-	{
-		id: 9,
-		image:
-			"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgQVz7ZuwxLvW708OradCP0Azc7ZzWmlR1nmHKr9w7FRJFE4xzRTDTWiSo7WG069zPFpaCH6xcNu5bvFUFs9b4O4wyC4-rmfk9fHbtuenCVeRj5XnHBvw49aFg3kedG2TrzI9mnmmUGIQ9hxVu34_GlBsMT8wgooJjC9dbZh9qJDeEpUHOdJv_TFOjJwLs/w400-h225/Blog%20Banner.png",
-		avatar: "https://plchldr.co/i/50x50",
-		category: "Travel",
-		date: "June 28, 2024",
-		title: "Belajar HTML dengan Mudah: Panduan Pemula untuk Coding Web",
-		content:
-			"Ingin tahu langkah pertama belajar HTML, di mana belajar, dan mengapa Anda harus melakukannya? Temukan jawabannya dalam panduan ini, serta rincian tentang waktu yang dibutuhkan, perangkat yang diperlukan, dan contoh kode HTML.",
-		link: "https://createdzvn.blogspot.com/2023/09/belajar-html-dengan-mudah-panduan.html",
+			"Seiring dengan kemajuan teknologi dan peningkatan kebutuhan akan kehadiran online, keahlian dalam pengembangan web semakin berharga. Salah satu langkah awal yang penting dalam perjalanan menjadi seorang web developer adalah memahami dan menguasai HTML (Hypertext Markup Language). Artikel ini akan membimbing Anda melalui dasar-dasar HTML, memberikan wawasan mendalam tentang bahasa markup ini, serta memberikan tips untuk memulai karier sebagai web developer.",
+		link: "https://createdzvn.blogspot.com/2023/08/belajar-html-panduan-lengkap-untuk.html",
 	},
 ];
 
@@ -160,51 +134,83 @@ export const menuItems = [
 
 export const portfolioItems = [
 	{
-		category: "Web Design",
+		category: "Web Development",
 		image: "https://plchldr.co/i/336x280",
-		title: "Web Design Project 1",
+		title: "Dashboard Admin Reservasi Hotel",
 		description: "Description of Web Design Project 1",
-		link: "/path/to/web-design1",
+		link: "https://github.com/zvn7/reactjs-admin-dashboard-reservasi-hotel",
 		technologies: [],
 	},
 	{
 		category: "Web Development",
 		image: "https://plchldr.co/i/336x280",
-		title: "Web Development Project 1",
+		title: "RestroFind",
 		description: "Description of Web Development Project 1",
-		link: "/path/to/web-development1",
-		technologies: ["JavaScript", "React", "Node.js"],
-	},
-	{
-		category: "Application",
-		image: "https://plchldr.co/i/336x280",
-		title: "Application Project 1",
-		description: "Description of Application Project 1",
-		link: "/path/to/application1",
-		technologies: ["JavaScript", "React", "Node.js"],
-	},
-	{
-		category: "Web Design",
-		image: "https://plchldr.co/i/336x280",
-		title: "Web Design Project 1",
-		description: "Description of Web Design Project 1",
-		link: "/path/to/web-design1",
-		technologies: ["JavaScript", "React", "Node.js"],
+		link: "https://frontend-dev-reactjs-ilham-maulana.vercel.app/",
+		technologies: [],
 	},
 	{
 		category: "Web Development",
 		image: "https://plchldr.co/i/336x280",
-		title: "Web Development Project 1",
+		title: "Dashboard Reservasi Hotel",
+		description: "Description of Web Design Project 1",
+		link: "https://github.com/zvn7/reservasi-hotel-laravel9",
+		technologies: [],
+	},
+	{
+		category: "Web Development",
+		image: "https://plchldr.co/i/336x280",
+		title: "Web layanan aplikasi ",
 		description: "Description of Web Development Project 1",
-		link: "/path/to/web-development1",
-		technologies: ["JavaScript", "React", "Node.js"],
+		link: "https://github.com/zvn7/web-layanan-app",
+		technologies: [],
+	},
+	{
+		category: "Web Development",
+		image: "https://plchldr.co/i/336x280",
+		title: "Move E-Learning Template ",
+		description: "Description of Web Design Project 1",
+		link: "https://zvn7.github.io/e-learning/",
+		technologies: [],
 	},
 	{
 		category: "Application",
 		image: "https://plchldr.co/i/336x280",
-		title: "Application Project 1",
+		title: "Reservasi Hotel Apps",
 		description: "Description of Application Project 1",
-		link: "/path/to/application1",
-		technologies: ["JavaScript", "React", "Node.js"],
+		link: "https://github.com/zvn7/react-native-reservasi-hotel",
+		technologies: [],
+	},
+	{
+		category: "Web Design",
+		image: "https://plchldr.co/i/336x280",
+		title: "Learning Management System",
+		description: "Description of Web Design Project 1",
+		link: "https://www.figma.com/file/3mkxkF3C0ODSbBeBYwmuW0/Learning-Management-System?type=design&node-id=0%3A1&mode=design&t=ZO0G6PjXJvAObFjA-1",
+		technologies: [],
+	},
+	{
+		category: "Web Design",
+		image: "https://plchldr.co/i/336x280",
+		title: "Movie Ticketing Apps ",
+		description: "Description of Web Development Project 1",
+		link: "https://www.figma.com/file/MpmLWMuie6FJvW0FDpMrlJ/Movie-ticketing-apps?type=design&node-id=0%3A1&mode=design&t=jE8nVWluaENp3vwF-1",
+		technologies: [],
+	},
+	{
+		category: "Web Design",
+		image: "https://plchldr.co/i/336x280",
+		title: "Travel Web Landing Page ",
+		description: "Description of Web Development Project 1",
+		link: "https://www.figma.com/file/2TXpxsyY3LzsWTXAUKZl9i/Travel-Website-Landing-Page?type=design&node-id=0%3A1&mode=design&t=m9h375w8PPJKZTue-1",
+		technologies: [],
+	},
+	{
+		category: "Web Design",
+		image: "https://plchldr.co/i/336x280",
+		title: "Food Recipe App ",
+		description: "Description of Web Development Project 1",
+		link: "https://www.figma.com/design/GTiYdKmp00Yk8GNSdBj7cm/Untitled?node-id=2-2676&t=BBqExhlMHMdxTwRj-1",
+		technologies: [],
 	},
 ];
